3a1fdc1812d6ea1415e2fee6ffe4a71e978a29a8
trishkare <emazasanura@gamil.com>
2018-08-17 12:36:43 +0300

    add function to calculate the score for each answer given

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
 // first step
-
 	function submitQuiz() {
 		console.log('submitted');
+
+// calculate each score for each answer given
+  function answerScore (qName) {
+    var radiosNo = document.getElementsByName(qName);
+
+      for (var i = 0, length = radiosNo.length; i < length; i++) {
+         if (radiosNo[i].checked) {
+// do something with radiosNo
+    	var answerValue = Number(radiosNo[i].value);
+  	}
+}
